414948ffdb8aec307435e8e138bbcbc36c1bd486
CVG42 <zap653@amerike.edu.mx>
2025-03-08 10:17:18 -0600

    Play sound button
    Created a button that plays sounds from the audio library just for testing

---
+++ GOLF/Assets/_Project/Scripts/Audio/PlaySoundButton.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Golf
+{
+    public class PlaySoundButton : MonoBehaviour
+    {
+        [SerializeField] private string _audioName;
+
+        private Button _button;
+
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+        }
+
+        private void Start()
+        {
+            _button.onClick.AddListener(PlaySFX);
+        }
+
+        public void PlaySFX() => AudioManager.Source.GetAudio(_audioName);
+    }
+}
