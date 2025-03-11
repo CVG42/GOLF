442ba683fe2ef3fdae106ae9150f1ac58e8a44fc
CVG42 <zap653@amerike.edu.mx>
2025-03-10 23:24:03 -0600

    Dictionary creation

--- GOLF/Assets/_Project/Scripts/Audio/AudioDatabase.cs
+++ GOLF/Assets/_Project/Scripts/Audio/AudioDatabase.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Golf
@@ -7,16 +9,16 @@ namespace Golf
     public class AudioDatabase : ScriptableObject
     {
         [SerializeField] AudioData[] _audioData;
+        private Dictionary<string, AudioData> _audioDataDictionary = new Dictionary<string, AudioData>();
+
+        private void OnEnable()
+        {
+            _audioDataDictionary = _audioData.ToDictionary(audioData => audioData.audioName, audioData => audioData);  
+        }
 
         public AudioClip GetAudio(string _audioName)
         {
-            for (int i = 0; i < _audioData.Length; i++)
-            {
-                if (_audioData[i].audioName == _audioName)
-                {
-                    return _audioData[i].audio; 
-                }
-            }
+            if (_audioDataDictionary.TryGetValue(_audioName, out AudioData audioData)) return audioData.audio;
 
             return null;
         }
