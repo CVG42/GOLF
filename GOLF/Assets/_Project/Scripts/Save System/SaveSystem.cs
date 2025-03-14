980c84fa7d17cd86d78b579ddface8d3fc893f5a
Eric I. Rosas <theericyea100@hotmail.com>
2025-03-14 15:55:09 -0600

    Fixed issue with SaveSystem
    Name of the class "EXAMPLE" changed, is now named PlayerData, in this script we can save the Data that we need of the game or the user.
    The missing space lines were added.
    Comments were deleted, no more spanish.
    Singleton Implemented and the interface too.
    The interface have one method for each variable we want to save or load.
    
    Also here is the evidence in video of the GIF player: https://drive.google.com/file/d/17kkzUvJ3JxJow1zFrhoFs_D_zUq7x71q/view?usp=drive_link

--- GOLF/Assets/_Project/Scripts/Save System/SaveSystem.cs
+++ GOLF/Assets/_Project/Scripts/Save System/SaveSystem.cs
@@ -1,26 +1,65 @@
 using System.IO;
 using UnityEngine;
 using Newtonsoft.Json;
+
 namespace Golf
 {
-    public static class SaveSystem
+    public class SaveSystem : MonoBehaviour, ISaveSource
     {
-        private static string savePath = Application.persistentDataPath + "SaveData";
+        private static SaveSystem _instance;
+        private string _savePath;
+        private GameData currentData;
 
-        public static void Save(GameData data)
-        { 
-            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
-            File.WriteAllText(savePath, json);
+        private void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this;
+                DontDestroyOnLoad(gameObject);
+                _savePath = Application.persistentDataPath + "/save.json";
+                currentData = Load();
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
         }
 
-        public static GameData Load()
+        public void Save(GameData data)
         {
-            if (!File.Exists(savePath))
-            { 
-                string json = File.ReadAllText(savePath);
+            currentData = data;
+            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+            File.WriteAllText(_savePath, json);
+        }
+
+        public GameData Load()
+        {
+            if (File.Exists(_savePath))
+            {
+                string json = File.ReadAllText(_savePath);
                 return JsonConvert.DeserializeObject<GameData>(json);
             }
-            return new GameData();
+            return new GameData { _playerVolume = 1.0f };
         }
+
+        public static SaveSystem Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    Debug.LogError("SaveSystem instance is not initialized.");
+                }
+                return _instance;
+            }
+        }
+        public int GetScore() => currentData._playerScore;
+        public void SetScore(int score) { currentData._playerScore = score; Save(currentData); }
+
+        public Vector2 GetPlayerPosition() => currentData._playerPosition;
+        public void SetPlayerPosition(Vector2 position) { currentData._playerPosition = position; Save(currentData); }
+
+        public float GetVolume() => currentData._playerVolume;
+        public void SetVolume(float volume) { currentData._playerVolume = volume; Save(currentData); }
     }
 }
