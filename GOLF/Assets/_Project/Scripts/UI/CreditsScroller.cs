d500ada2404d468554e5a3db4cc41b6bd683893a
Eric I. Rosas <theericyea100@hotmail.com>
2025-03-15 15:59:07 -0600

    Fixed Credit Screen
    Method StarScrolling restructured.
    Script cleaned of empty lines.

--- GOLF/Assets/_Project/Scripts/UI/CreditsScroller.cs
+++ GOLF/Assets/_Project/Scripts/UI/CreditsScroller.cs
@@ -2,26 +2,26 @@ using UnityEngine;
 using DG.Tweening;
 
 namespace Golf
-{
-    
+{ 
     public class CreditsScroller : MonoBehaviour
     {
-        [SerializeField] public RectTransform TextPosition;
+        [SerializeField] private RectTransform _textRectTransform;
         [SerializeField] private float _duration = 30f;
         [SerializeField] private float _initialPosition = -1000f;
         [SerializeField] private float _endPosition = 1000f;
 
-        void Start()
+        private void Start()
         {
             StartScrolling();
         }
 
-        void StartScrolling()
-        {         
-            TextPosition.anchoredPosition = new Vector2(TextPosition.anchoredPosition.x, _initialPosition);
-            TextPosition.DOAnchorPosY(_endPosition, _duration)
+        private void StartScrolling()
+        {
+            _textRectTransform
+                .DOAnchorPosY(_endPosition, _duration)
+                .From(new Vector2(_textRectTransform.anchoredPosition.x, _initialPosition))
                 .SetEase(Ease.Linear)
-                .OnComplete(StartScrolling);
+                .OnComplete(StartScrolling);           
         }
     }
 }
