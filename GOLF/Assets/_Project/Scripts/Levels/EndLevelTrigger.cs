b46a5a6d56d8868a6382d864cb7ed92856bd7468
CVG42 <zap653@amerike.edu.mx>
2025-03-16 10:41:58 -0600

    End Level Trigger script PlayerPrefs
    Modified the end level trigger script so it sets the specific value to player prefs and the next level can be unlocked.

--- GOLF/Assets/_Project/Scripts/Levels/EndLevelTrigger.cs
+++ GOLF/Assets/_Project/Scripts/Levels/EndLevelTrigger.cs
@@ -4,12 +4,14 @@ namespace Golf
 {
     public class EndLevelTrigger : MonoBehaviour
     {
+        [SerializeField] private int _levelID;
         [SerializeField] private string _sceneName;
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.CompareTag("Player"))
             {
+                PlayerPrefs.SetInt("Lv" + _levelID, 1);
                 LevelManager.Source.LoadScene(_sceneName);
             }
         }
