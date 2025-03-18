d95d8d7ad1ab999123a78711cae9f832348bee6d
Cristina Vera <99357695+CVG42@users.noreply.github.com>
2025-03-18 10:13:06 -0600

    Tutorial mechanics (#33)
    * Tutorial art addition
    
    Added missing art
    
    * Tilemaps creation
    
    * InputManager and Player prefabs creation
    
    * Tutorial and Testing scene modifications
    
    Added tutorial scene to the build settings and changed its art. Modified some things from the testing scene.
    
    * Level selector script addition
    
    Made a level selection script so it unlocks each level depending on the game progression. It only works with PlayerPrefs for now, can be changed later on to work with the json Save system.
    
    * End Level Trigger script PlayerPrefs
    
    Modified the end level trigger script so it sets the specific value to player prefs and the next level can be unlocked.
    
    * Small script change
    
    Changed the rigidbody GetComponent to Awawke instead of Start
    
    * Small tutorial scene changes
    
    * Minor changes

---
+++ GOLF/Assets/_Project/Scripts/Levels/Tutorial/LevelSelector.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+namespace Golf
+{
+    public class LevelSelector : MonoBehaviour
+    {
+        [SerializeField] private GameObject _levelLayout;
+        [SerializeField] private string _sceneName;
+        [SerializeField] private bool _isUnlocked;
+        [SerializeField] private bool _isFirstLevel = false;
+
+        private int _levelID;
+
+        private void Start()
+        {
+            UpdateUnlockStatus();           
+        }
+
+        private void UpdateUnlockStatus()
+        {
+            _levelID = int.Parse(gameObject.name) - 1;
+            _isUnlocked = PlayerPrefs.GetInt($"Lv{_levelID}") > 0;
+            _levelLayout.SetActive(!_isUnlocked);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+            { 
+                LevelSelected(); 
+            }
+        }
+
+        private void LevelSelected()
+        {
+            if (_isUnlocked || _isFirstLevel) 
+            {
+                LevelManager.Source.LoadScene(_sceneName);
+            }
+        }
+    }
+}
