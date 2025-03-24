354070901e785588286b7e21e94093c2c4df189f
Ivan Herrera <ivan.herrera.m@outlook.com>
2025-03-24 12:20:47 -0600

    Fixed conflicts

--- GOLF/Assets/_Project/Scripts/Input/IInputSource.cs
+++ GOLF/Assets/_Project/Scripts/Input/IInputSource.cs
@@ -5,8 +5,8 @@ namespace Golf
 {
     public interface IInputSource
     {
-        event Action OnConfirmButtonPressed { get; set; }
-        event Action<ACTION_STATE> OnActionChange { get; set; }
+        event Action OnConfirmButtonPressed;
+        event Action<ACTION_STATE> OnActionChange;
         ACTION_STATE CurrentAction { get; }
 
         void ChangeAction(ACTION_STATE action);
