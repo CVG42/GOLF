1262982a65027b25ff06513b8e64e812af346c11
Eric I. Rosas <theericyea100@hotmail.com>
2025-03-25 09:39:43 -0600

    Audio settings ready
    Audio settings ready for use it in a Slider.

--- GOLF/Assets/_Project/Scripts/Audio/AudioManager.cs
+++ GOLF/Assets/_Project/Scripts/Audio/AudioManager.cs
@@ -3,44 +3,40 @@ using UnityEngine.Audio;
 
 namespace Golf
 {
-    public class AudioManager : Singleton<IAudioSource>, IAudioSource
+    public class AudioManager : Singleton<AudioManager>, IAudioSource
     {
         [SerializeField] private AudioDatabase _audioDatabase;
         [SerializeField] private AudioMixer _sfxMixer;
+        [SerializeField] private AudioMixer _musicMixer;
         [SerializeField] private AudioSource _sfxAudioSource;
 
         public float CurrentVolume { get; private set; }
 
         private void Start()
         {
-            GetAudioPlayerPrefKeys();
+            LoadAudioSettings();
+        }
+
+        public void LoadAudioSettings()
+        {
+            float sfxVolume = SaveSystem.Source.GetSFXVolume();
+            float musicVolume = SaveSystem.Source.GetMusicVolume();
+
+            _sfxMixer.SetFloat("sfx_vol", sfxVolume);
+            _musicMixer.SetFloat("music_vol", musicVolume);
         }
 
         public void SetSFXVolume(bool setVolumeUp)
         {
-            CurrentVolume = setVolumeUp ? Mathf.Max(-80, CurrentVolume - 20):Mathf.Min(0, CurrentVolume + 20);
+            CurrentVolume = setVolumeUp ? Mathf.Max(-80, CurrentVolume - 20) : Mathf.Min(0, CurrentVolume + 20);
             _sfxMixer.SetFloat("sfx_vol", CurrentVolume);
-            PlayerPrefs.SetFloat("sfx_vol", CurrentVolume);
-        }
-
-        private void GetAudioPlayerPrefKeys()
-        {
-            if (PlayerPrefs.HasKey("sfx_vol"))
-            {
-                CurrentVolume = PlayerPrefs.GetFloat("sfx_vol", CurrentVolume);
-                _sfxMixer.SetFloat("sfx_vol", CurrentVolume);
-            }
-            else CurrentVolume = 0;
+            SaveSystem.Source.SetSFXVolume(CurrentVolume);
         }
 
         public void BallHitSFX() => _sfxAudioSource.PlayOneShot(_audioDatabase.GetAudio("BallHitSFX"));
-
         public void PauseSFX() => _sfxAudioSource.PlayOneShot(_audioDatabase.GetAudio("PauseSFX"));
-
         public void ButtonClickSFX() => _sfxAudioSource.PlayOneShot(_audioDatabase.GetAudio("ButtonClickSFX"));
-
         public void ButtonSelectHoverSFX() => _sfxAudioSource.PlayOneShot(_audioDatabase.GetAudio("ButtonSelectHoverSFX"));
-      
-        public void SetAngleSFX() => _sfxAudioSource.PlayOneShot(_audioDatabase.GetAudio("SetAngleSFX"));       
+        public void SetAngleSFX() => _sfxAudioSource.PlayOneShot(_audioDatabase.GetAudio("SetAngleSFX"));
     }
 }
