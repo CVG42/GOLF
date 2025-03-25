22e695eec1d5f0c2dd7578c862c85ddc10ff7ad1
CVG42 <zap653@amerike.edu.mx>
2025-03-25 10:49:49 -0600

    Changed script to lock level one

--- GOLF/Assets/_Project/Scripts/Levels/Tutorial/LevelSelector.cs
+++ GOLF/Assets/_Project/Scripts/Levels/Tutorial/LevelSelector.cs
@@ -5,6 +5,7 @@ namespace Golf
     public class LevelSelector : MonoBehaviour
     {
         [SerializeField] private GameObject _levelLayout;
+        [SerializeField] private GameObject _previouLevelLock;
         [SerializeField] private string _sceneName;
         [SerializeField] private bool _isUnlocked;
         [SerializeField] private bool _isFirstLevel = false;
@@ -21,12 +22,13 @@ namespace Golf
             _levelID = int.Parse(gameObject.name) - 1;
             _isUnlocked = PlayerPrefs.GetInt($"Lv{_levelID}") > 0;
             _levelLayout.SetActive(!_isUnlocked);
+            _previouLevelLock.SetActive(_isUnlocked);
         }
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.CompareTag("Player"))
-            { 
+            {
                 LevelSelected(); 
             }
         }
