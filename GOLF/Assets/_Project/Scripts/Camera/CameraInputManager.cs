ea7838093e60c14e948fc28b0e9328bf7960e7ef
Ivan Alejandro Antonio Ruiz <99371089+IXAL08@users.noreply.github.com>
2025-04-09 10:32:37 -0600

    Changes in camera system
    Change in the size and function in the camera

--- GOLF/Assets/_Project/Scripts/Camera/CameraInputManager.cs
+++ GOLF/Assets/_Project/Scripts/Camera/CameraInputManager.cs
@@ -7,53 +7,45 @@ namespace Golf
     public class CameraInputManager : MonoBehaviour
     {
         [SerializeField] private Transform _player;
-        [SerializeField] private GameObject _movementFrame;
         [SerializeField] private Camera _camera;
         [SerializeField] private CameraInputConfigurations _cameraInputConfigurations;
         [SerializeField] private float _followSmoothTime = 0.1f;
-        [SerializeField] private float _zoomTime = 0.4f;
-        
-        private bool _isLocked = true;
-        private bool _enableFollow = true;
+
         private Vector3 _velocityCamera = Vector3.zero;
-        private Sequence _currentTweenSequence;
 
-        private void Start()
+        private void OnEnable()
         {
-            InputManager.Source.OnToggleCameraMode += ToggleLockCamera;
+            InputManager.Source.OnMoveCamera += MoveCamera;
         }
-
         private void OnDestroy()
         {
-            InputManager.Source.OnToggleCameraMode -= ToggleLockCamera;
             InputManager.Source.OnMoveCamera -= MoveCamera;
         }
 
         private void FixedUpdate()
         {
-            if (_enableFollow)
+            if (InputManager.Source.IsLocking == true)
             {
                 FollowPlayer();
             }
         }
 
-        private void ToggleLockCamera(bool state)
+        private void Update()
         {
-            _isLocked = state;
-            if (!_isLocked)
+            if (CheckBallAction(InputManager.Source.CurrentActionState))
             {
-                ZoomOutToCameraMode();
+                InputManager.Source.IsLocking = true;
             }
             else
             {
-                ZoomInToFollowPlayer();
+                InputManager.Source.IsLocking = false;
             }
         }
 
         private void FollowPlayer()
         {
             transform.position = Vector3.SmoothDamp(transform.position, GetFollowPlayerPosition(), ref _velocityCamera, _followSmoothTime);
-            transform.position = GetClampedPlayerModePosition(transform.position);
+            transform.position = GetClampedCameraPosition(transform.position);
         }
         
         private Vector3 GetFollowPlayerPosition()
@@ -61,54 +53,31 @@ namespace Golf
             return _player.position + new Vector3(0f, 1f, -10f);
         }
 
-        private Vector3 GetClampedPlayerModePosition(Vector3 position)
-        {
-            var clampedPosition = position;
-            clampedPosition.x = Mathf.Clamp(clampedPosition.x, _cameraInputConfigurations.XOffsetNegative, _cameraInputConfigurations.XOffsetPositive);
-            clampedPosition.y = Mathf.Clamp(clampedPosition.y, _cameraInputConfigurations.YOffsetNegative, _cameraInputConfigurations.YOffsetPositive);
-            
-            return clampedPosition;
-        }
-
         private void MoveCamera(Vector2 moveDirection)
         {
             Vector3 move = 20f * Time.deltaTime * (Vector3)moveDirection;
             transform.position += move;
-            transform.position = GetClampedCameraModePosition(transform.position);
+            transform.position = GetClampedCameraPosition(transform.position);
         }
 
-        private Vector3 GetClampedCameraModePosition(Vector3 position)
+        private Vector3 GetClampedCameraPosition(Vector3 position)
         {
             var clampedPosition = position;
-            clampedPosition.x = Mathf.Clamp(clampedPosition.x, _cameraInputConfigurations.ZXOffsetNegative, _cameraInputConfigurations.ZXOffsetPositive);
-            clampedPosition.y = Mathf.Clamp(clampedPosition.y, _cameraInputConfigurations.ZYOffsetNegative, _cameraInputConfigurations.ZYOffsetositive);
-            
+            clampedPosition.x = Mathf.Clamp(clampedPosition.x, _cameraInputConfigurations.XOffsetNegative, _cameraInputConfigurations.XOffsetPositive);
+            clampedPosition.y = Mathf.Clamp(clampedPosition.y, _cameraInputConfigurations.YOffsetNegative, _cameraInputConfigurations.YOffsetPositive);
             return clampedPosition;
         }
 
-        private void ZoomInToFollowPlayer() 
+        private bool CheckBallAction(ActionState state)
         {
-            InputManager.Source.OnMoveCamera -= MoveCamera;
-            
-            _movementFrame.SetActive(false);
-            _currentTweenSequence?.Kill();
-
-            _currentTweenSequence = DOTween.Sequence();
-            _currentTweenSequence.Append(_camera.DOOrthoSize(7, _zoomTime).SetEase(Ease.OutQuad));
-            _currentTweenSequence.Join(transform.DOMove(GetClampedPlayerModePosition(GetFollowPlayerPosition()), _zoomTime).SetEase(Ease.OutQuad));
-            _currentTweenSequence.AppendCallback(() => _enableFollow = true);
-        }
-
-        private void ZoomOutToCameraMode()
-        {
-            _enableFollow = false;
-            _movementFrame.SetActive(true);
-            _currentTweenSequence?.Kill();
-            
-            _currentTweenSequence = DOTween.Sequence();
-            _currentTweenSequence.Append(_camera.DOOrthoSize(10, _zoomTime).SetEase(Ease.OutQuad));
-            _currentTweenSequence.Join(transform.DOMove(GetClampedCameraModePosition(GetFollowPlayerPosition()), _zoomTime).SetEase(Ease.OutQuad));
-            _currentTweenSequence.AppendCallback(() => InputManager.Source.OnMoveCamera += MoveCamera);
+            if (ActionState.Moving == state) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
