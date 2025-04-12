d0b1f252f51f8bae790db6284a65d852a9b91757
Ivan Alejandro Antonio Ruiz <99371089+IXAL08@users.noreply.github.com>
2025-04-12 10:44:54 -0600

    Changes in camera system (#94)
    * Changes in camera system
    
    Change in the size and function in the camera
    
    * Minor change
    
    Added a new CameraMovementFrame sprite and change the name of SO
    
    * Rebase adjustments

--- GOLF/Assets/_Project/Scripts/Camera/CameraInputConfigurations.cs
+++ GOLF/Assets/_Project/Scripts/Camera/CameraInputConfigurations.cs
@@ -5,13 +5,9 @@ namespace Golf
     [CreateAssetMenu(fileName = "CameraThresholdsData", menuName = "Scriptables/CameraThresholdsData", order = 1)]
     public class CameraInputConfigurations : ScriptableObject
     {
-        public float XOffsetPositive;
-        public float XOffsetNegative;
-        public float YOffsetPositive;
-        public float YOffsetNegative;
-        public float ZXOffsetPositive;
-        public float ZXOffsetNegative;
-        public float ZYOffsetositive;
-        public float ZYOffsetNegative;
+        public float RightThresholdMap;
+        public float LeftThresholdMap;
+        public float UpThresholdMap;
+        public float DownThresholdMap;
     }
 }
