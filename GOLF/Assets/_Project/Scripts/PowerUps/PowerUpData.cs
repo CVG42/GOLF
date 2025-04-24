ea4756e1adea0a74a45ceca8eda63178312c8509
CVG42 <zap653@amerike.edu.mx>
2025-04-23 18:10:46 -0600

    PowerUpData script change
    Modified the script to the requested changes, so now the variables are private and exposed with getters

--- GOLF/Assets/_Project/Scripts/PowerUps/PowerUpData.cs
+++ GOLF/Assets/_Project/Scripts/PowerUps/PowerUpData.cs
@@ -5,9 +5,14 @@ namespace Golf
     [CreateAssetMenu(fileName = "NewPowerUp", menuName = "PowerUps/Powerup Data")]
     public class PowerUpData : ScriptableObject
     {
-        public float ballMass;
-        public float ballAngularDrag;
-        public Sprite ballSprite;
-        public bool canDestroyWalls;
+        [SerializeField] private float _ballMass;
+        [SerializeField] private float _ballAngularDrag;
+        [SerializeField] private Sprite _ballSprite;
+        [SerializeField] private bool _canDestroyWalls;
+
+        public float BallMass => _ballMass;
+        public float BallAngularDrag => _ballAngularDrag;
+        public Sprite BallSprite => _ballSprite;
+        public bool CanDestroyWalls => _canDestroyWalls;
     }
 }
