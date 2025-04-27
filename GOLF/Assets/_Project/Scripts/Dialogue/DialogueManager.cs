9c898557e42a7c3970fcc671ad6c3f714443468d
Ivan Alejandro Antonio Ruiz <99371089+IXAL08@users.noreply.github.com>
2025-04-27 05:25:25 -0600

    Fire ball cave (#115)
    * Fireball Cave Scene
    
    * New dialogue cinematic position created
    
    * Minor changes
    
    * Dialogue and Input manager adjustment
    
    * OnDestroy unsuscribed event

--- GOLF/Assets/_Project/Scripts/Dialogue/DialogueManager.cs
+++ GOLF/Assets/_Project/Scripts/Dialogue/DialogueManager.cs
@@ -12,20 +12,31 @@ namespace Golf
         [SerializeField] private Image _characterImage;
         [SerializeField] private TextMeshProUGUI _characterName;
         [SerializeField] private TextMeshProUGUI _dialogueArea;
+        [SerializeField] private Button _dialogueButton;
         [SerializeField] private float _typingSpeed = 0.2f;
-        [SerializeField] private GameObject _dialoguePrefab;
+        [SerializeField] private Canvas _dialogueFormat;
         [SerializeField] private Action _onDialogueEnd;
-
-        private bool isTyping = false;
-        private bool skipTyping = false;
-        private string currentSentence = "";
+        
+        private bool _isTyping = false;
+        private bool _skipTyping = false;
+        private string _currentSentence = "";
         private readonly Queue<DialogueLine> _lines = new Queue<DialogueLine>();
-             
+
+        private void Start()
+        {
+            InputManager.Source.OnConfirmButtonPressed += NextDialogue;
+        }
+
+        private void OnDestroy()
+        {
+            InputManager.Source.OnConfirmButtonPressed -= NextDialogue;
+        }
+
         public void StartDialogue(Dialogue dialogue, Action onDialogueEnd)
         {
             InputManager.Source.Disable();
             _onDialogueEnd = onDialogueEnd;
-            _dialoguePrefab.gameObject.SetActive(true);
+            _dialogueFormat.enabled = true;
             _lines.Clear();
 
             foreach (DialogueLine dialogueline in dialogue.DialogueLines)
@@ -38,9 +49,9 @@ namespace Golf
 
         public void DisplayNextDialogueLine()
         {
-            if (isTyping)
+            if (_isTyping)
             {
-                skipTyping = true;
+                _skipTyping = true;
                 return;
             }
 
@@ -60,16 +71,14 @@ namespace Golf
 
         private async void TypeSentence(DialogueLine dialogueline)
         {
-            isTyping = true;
-            skipTyping = false;
-            currentSentence = dialogueline.Line;
-            
-            AudioManager.Source.PlayOneShot(dialogueline.Character.AudioName);
+            _isTyping = true;
+            _skipTyping = false;
+            _currentSentence = dialogueline.Line;
 
             _dialogueArea.text = "";
             foreach (char letter in dialogueline.Line.ToCharArray())
             {
-                if (skipTyping)
+                if (_skipTyping)
                 {
                     _dialogueArea.text = dialogueline.Line;
                     break;
@@ -79,14 +88,19 @@ namespace Golf
                 AudioManager.Source.TypingSFX();
                 await UniTask.Delay(TimeSpan.FromSeconds(_typingSpeed), DelayType.DeltaTime);
             }
-            isTyping = false;
+            _isTyping = false;
         }
 
         private void EndDialogue()
         {
-            _dialoguePrefab.gameObject.SetActive(false);
+            _dialogueFormat.enabled = false;
             _onDialogueEnd?.Invoke();
             InputManager.Source.Enable();
+        }
+
+        private void NextDialogue()
+        {
+            _dialogueButton.onClick.Invoke();
         }
     }
 }
