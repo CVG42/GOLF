5ade68502513eab4507c96322591ed78928a64e2
Ivan Alejandro Antonio Ruiz <99371089+IXAL08@users.noreply.github.com>
2025-04-29 10:38:21 -0600

    Loading bar added (#122)

--- GOLF/Assets/_Project/Scripts/Levels/LevelManager.cs
+++ GOLF/Assets/_Project/Scripts/Levels/LevelManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using Cysharp.Threading.Tasks;
 using UnityEngine.SceneManagement;
 using DG.Tweening;
+using UnityEngine.UI;
 
 namespace Golf
 {
@@ -9,16 +10,22 @@ namespace Golf
     {
         [SerializeField] private CanvasGroup _levelTransitionCanvasGroup;
         [SerializeField] private CanvasGroup _spawnTransitionCanvasGroup;
+        [SerializeField] private GameObject _loadingBar;
+        [SerializeField] private Transform[] _loadingBalls;
 
         private Sequence _currentTweenSequence;
 
         private async UniTask LoadSceneAsync(string sceneName)
         {
             _levelTransitionCanvasGroup.gameObject.SetActive(true);
+            RotateBalls();
+            _loadingBar.SetActive(true);
             await _levelTransitionCanvasGroup.DOFade(1, 2).AsyncWaitForCompletion();
             await SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single).ToUniTask();
+            _loadingBar.SetActive(false);
             await _levelTransitionCanvasGroup.DOFade(0, 1.5f).AsyncWaitForCompletion();
             _levelTransitionCanvasGroup.gameObject.SetActive(false);
+            StopBalls();
         }
 
         public void LoadScene(string sceneName) 
@@ -41,6 +48,22 @@ namespace Golf
         private void ResetTransitionPosition()
         {
             _spawnTransitionCanvasGroup.transform.position = new Vector2(-960, _spawnTransitionCanvasGroup.transform.position.y);
+        }
+
+        private void RotateBalls()
+        {
+            foreach (var ball in _loadingBalls)
+            {
+                ball.DORotate(new Vector3(0, 0, -360f), 1.5f, RotateMode.FastBeyond360).SetEase(Ease.Linear).SetLoops(-1, LoopType.Restart);
+            }
+        }
+
+        private void StopBalls()
+        {
+            foreach (var ball in _loadingBalls)
+            {
+                ball.DOPause();
+            }
         }
     }
 }
