6c1c8d9c4e666417775aca71e23e996bcc84b6e8
CVG42 <zap653@amerike.edu.mx>
2025-05-01 21:21:57 -0600

    Small script adjustments

--- GOLF/Assets/_Project/Scripts/Levels/EndLevelTrigger.cs
+++ GOLF/Assets/_Project/Scripts/Levels/EndLevelTrigger.cs
@@ -6,12 +6,21 @@ namespace Golf
     {
         [SerializeField] private int _levelID;
         [SerializeField] private string _sceneName;
+        [SerializeField] private bool _isTutorialLevel = false;
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.CompareTag("Player"))
             {
-                SaveSystem.Source.SetLevelCleared(_levelID);
+                if (!_isTutorialLevel)
+                {
+                    SaveSystem.Source.SetLevelCleared(_levelID);
+                }
+                else
+                {
+                    SaveSystem.Source.MarkTutorialAsCleared();
+                }
+
                 AudioManager.Source.FadeOutMusic();
                 LevelManager.Source.LoadScene(_sceneName);
             }
