c24fa09e23d093328fe1001eab580b76f4c99cac
Cristina Vera <99357695+CVG42@users.noreply.github.com>
2025-05-17 11:27:55 -0600

    Fire power up effect (#188)
    * PowerUpsTest scene change to test effect
    
    * CameraZoomTrigger script creation
    
    * Wall script change
    
    * Scene updates
    
    * Ice sprite asset and animations creation
    
    * Wall script changes
    
    * Scene final adjustments

--- GOLF/Assets/_Project/Scripts/Objects/Wall.cs
+++ GOLF/Assets/_Project/Scripts/Objects/Wall.cs
@@ -1,3 +1,6 @@
+using System;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using UnityEngine;
 
 namespace Golf
@@ -6,6 +9,18 @@ namespace Golf
     {
         [SerializeField] private Collider2D _wallCollider;
         [SerializeField] private int _wallHardness;
+
+        [SerializeField] private float _slowTimeScale = 0.1f;
+        [SerializeField] private float _slowdownDuration = 0.4f;
+
+        [SerializeField] private float _cameraShakeDuration = 1f;
+        [SerializeField] private AnimationCurve _curve;
+
+        [SerializeField] private Animator[] _wallAnimators;
+        [SerializeField] private string _animatorParameter;
+
+        private Vector3 _originalPosition;
+        private bool _isShaking = false;
 
         private void Start()
         {
@@ -22,11 +37,73 @@ namespace Golf
             _wallCollider.isTrigger = powerUpData.Hardness > _wallHardness;
         }
 
-        private void OnTriggerEnter2D(Collider2D collision)
+        private async void OnTriggerEnter2D(Collider2D collision)
         {
-            if (collision.CompareTag("Player"))
+            if (!collision.CompareTag("Player") || _isShaking) return;
+
+            _isShaking = true;
+
+            float originalTimeScale = Time.timeScale;
+            float originalFixedDeltaTime = Time.fixedDeltaTime;
+
+            Time.timeScale = _slowTimeScale;
+            Time.fixedDeltaTime = originalFixedDeltaTime * _slowTimeScale;
+
+            PlayAnimation();
+            var shakeTask = ShakeCamera();
+
+            await UniTask.Delay(TimeSpan.FromSeconds(_slowdownDuration), DelayType.UnscaledDeltaTime);
+
+            Time.timeScale = originalTimeScale;
+            Time.fixedDeltaTime = originalFixedDeltaTime;
+
+            await shakeTask;
+
+            await UniTask.Delay(TimeSpan.FromSeconds(1f), DelayType.UnscaledDeltaTime);
+
+            DeactivateObjects(false);
+            _isShaking = false;
+        }
+
+        private async UniTask ShakeCamera()
+        {
+            Camera camera = Camera.main;
+            
+            if (camera == null) return;
+
+            Transform cameraTransform = camera.transform;
+            _originalPosition = cameraTransform.position;
+
+            float elapsed = 0f;
+
+            while (elapsed < _cameraShakeDuration)
             {
-                gameObject.SetActive(false);
+                elapsed += Time.unscaledDeltaTime;
+                float strength = _curve.Evaluate(elapsed / _cameraShakeDuration);
+                Vector2 offset = UnityEngine.Random.insideUnitCircle * strength;
+                cameraTransform.position = _originalPosition + new Vector3(offset.x, offset.y, 0f);
+                await UniTask.Yield(PlayerLoopTiming.Update);
+            }
+
+            cameraTransform.position = _originalPosition;
+        }
+
+        private void PlayAnimation()
+        {
+            for (int i = 0; i < _wallAnimators.Length; i++)
+            {
+                int animationIndex = (i == 0) ? 1 : 2;
+                _wallAnimators[i].SetInteger(_animatorParameter, animationIndex);
+            }
+        }
+
+        private void DeactivateObjects(bool state)
+        {
+            gameObject.SetActive(state);
+
+            for (int i = 0; i < _wallAnimators.Length; i++)
+            {
+                _wallAnimators[i].gameObject.SetActive(state);
             }
         }
     }
