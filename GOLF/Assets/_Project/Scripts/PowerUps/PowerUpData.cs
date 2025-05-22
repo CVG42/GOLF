7aecc41545bf0e4689eadde444e7b9346b6e4107
CVG42 <zap653@amerike.edu.mx>
2025-05-22 10:11:12 -0600

    PowerUpData changes
    Added a PowerUpType to identify which type of powerup is being used and a bool to know if it has an effect

--- GOLF/Assets/_Project/Scripts/PowerUps/PowerUpData.cs
+++ GOLF/Assets/_Project/Scripts/PowerUps/PowerUpData.cs
@@ -5,14 +5,26 @@ namespace Golf
     [CreateAssetMenu(fileName = "NewPowerUp", menuName = "PowerUps/Powerup Data")]
     public class PowerUpData : ScriptableObject
     {
+        [SerializeField] private PowerUpType _powerUpType;
         [SerializeField] private float _ballMass;
         [SerializeField] private float _ballAngularDrag;
         [SerializeField] private Sprite _ballSprite;
         [SerializeField] private int _hardness;
+        [SerializeField] private bool _hasEffect;
 
+        public PowerUpType PowerUpType => _powerUpType;
         public float BallMass => _ballMass;
         public float BallAngularDrag => _ballAngularDrag;
         public Sprite BallSprite => _ballSprite;
         public int Hardness => _hardness;
+        public bool HasEffect => _hasEffect;
+    }
+
+    public enum PowerUpType
+    {
+        None,
+        Ice,
+        Fire,
+        Stone,
     }
 }
