aa91b88ade835b99e254ce12ad0157dc4283761d
Ivan Alejandro Antonio Ruiz <99371089+IXAL08@users.noreply.github.com>
2025-06-06 14:21:49 -0600

    Main menu trigger hotfix

--- GOLF/Assets/_Project/Scripts/Levels/Tutorial/MainMenuBack.cs
+++ GOLF/Assets/_Project/Scripts/Levels/Tutorial/MainMenuBack.cs
@@ -4,7 +4,6 @@ namespace Golf
 {
     public class MainMenuBack : MonoBehaviour
     {
-        private bool _isOnTrigger = false;
         private IInputSource _inputSource;
 
         private void Awake()
@@ -19,22 +18,21 @@ namespace Golf
 
         private void BackToMenu()
         {
+            _inputSource.OnConfirmButtonPressed -= BackToMenu;
             LevelManager.Source.LoadScene("MainMenu");
         }
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            if (collision.CompareTag("Player") && _isOnTrigger == false)
+            if (collision.CompareTag("Player"))
             {
                 _inputSource.OnConfirmButtonPressed += BackToMenu;
-                _isOnTrigger = true;
             }
         }
 
         private void OnTriggerExit2D(Collider2D collision)
         {
             _inputSource.OnConfirmButtonPressed -= BackToMenu;
-            _isOnTrigger = false;
         }
     }
 }
